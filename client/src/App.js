f63e3cc90fc9623245fa32efb70e9d4c1ed1bffb
Jordan <Jordanholm@gmail.com>
2022-12-21 18:48:13 -0700

    Project created, client side created

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./components/home/Home";
+
+const App = () => (
+  <>
+
+    <Routes>
+      <Route path="/" element={ <Home /> }/>
+    </Routes>
+  </>
+)
+
+export default App;
